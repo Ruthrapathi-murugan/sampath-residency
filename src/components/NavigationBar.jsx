440809fe1d3730df97388c9ba66bdde6b8de1c59
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2024-09-30 01:11:33 +0530

    Navigation change

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';  // Import Link for navigation
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">Sampath Residency</Navbar.Brand> {/* Use Link for brand too */}
+        <Navbar.Brand as={Link} to="/">Sampath Residency</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            {/* Use Link instead of href */}
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/rooms">Rooms</Nav.Link>
-            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/todo "> Things To Do</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/" className="nav-item">Home</Nav.Link>
+            <Nav.Link as={Link} to="/rooms" className="nav-item">Rooms</Nav.Link>
+            <Nav.Link as={Link} to="/about" className="nav-item">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/todo" className="nav-item">Things To Do</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav-item">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
